8b3038063918b45e135c09d97bc14949cf9edf09
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2023-11-10 12:30:58 -0500

    add wheel speed

---
+++ quest-4/code/blink/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "/Users/noahrobitshek/esp/esp-idf/components/bootloader/subproject"
+  "/Users/noahrobitshek/esp/EC 444/Team2-Gilbert-Robitshek/quest-4/code/blink/build/bootloader"
+  "/Users/noahrobitshek/esp/EC 444/Team2-Gilbert-Robitshek/quest-4/code/blink/build/bootloader-prefix"
+  "/Users/noahrobitshek/esp/EC 444/Team2-Gilbert-Robitshek/quest-4/code/blink/build/bootloader-prefix/tmp"
+  "/Users/noahrobitshek/esp/EC 444/Team2-Gilbert-Robitshek/quest-4/code/blink/build/bootloader-prefix/src/bootloader-stamp"
+  "/Users/noahrobitshek/esp/EC 444/Team2-Gilbert-Robitshek/quest-4/code/blink/build/bootloader-prefix/src"
+  "/Users/noahrobitshek/esp/EC 444/Team2-Gilbert-Robitshek/quest-4/code/blink/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/Users/noahrobitshek/esp/EC 444/Team2-Gilbert-Robitshek/quest-4/code/blink/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "/Users/noahrobitshek/esp/EC 444/Team2-Gilbert-Robitshek/quest-4/code/blink/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
